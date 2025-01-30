89642a13a7152ec2f9eaa44c7614fde06a918e10
Arunabh <96439469+Arunabh-P@users.noreply.github.com>
2025-01-30 23:46:57 +0530

    next config change

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const config: import('next').NextConfig = {
-  distDir: 'build',
   compress: false,
   reactStrictMode: true,
+  output: 'standalone',
+
   images: {
     remotePatterns: [
       {
